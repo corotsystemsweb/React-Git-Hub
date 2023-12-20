e966024ddc1d51e8f21b722e254396ffc795e962
Ankita <ankitaojha1611@gmail.com>
2023-12-20 12:58:22 +0530

    Course Page Update

--- upic/src/views/Course.js
+++ upic/src/views/Course.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ServiceImg from "../assets/images/service.png";
+import CourseImg from "../assets/images/courses.jpeg";
 import PageTopBanner from '../components/PageTopBanner';
 
 const Course = () => {
    return (
       <>
          <PageTopBanner pageTitle={"Course Details"} />
-         <section>
+         <section className='mt-5 mb-5'>
             <div className="container">
                <div className="row">
                   <div className="col-md-12 text-center">
@@ -21,20 +21,14 @@ const Course = () => {
                <div className="row mt-5">
                   <div className="col-md-6 ">
                      <div className="about_box mt-5">
-                        <figure><img src={ServiceImg} alt="About Us" className='img-shadow' /></figure>
+                        <figure><img src={CourseImg} alt="About Us" className='img-shadow' /></figure>
                      </div>
                   </div>
                   <div className="col-md-6">
-                     <p>
-                        Welcome to Uday Pratap Inter College, a beacon of excellence in education and character development.
-                        Established with a vision to nurture young minds and foster holistic growth, Uday Pratap Inter College stands as a
-                        testament to educational innovation and commitment. Our dedicated team of educators strives to provide a dynamic
-                        learning environment that encourages curiosity, critical thinking, and creativity. Rooted in values and tradition
-                        , our institution not only imparts knowledge but also instills a sense of responsibility, discipline, and compassion
-                        in our students. At Uday Pratap Inter College, we believe in empowering every student to discover their unique
-                        potential, equipping them with the skills needed to navigate an ever-changing world. Our commitment to academic
-                        excellence is complemented by a rich tapestry of co-curricular activities, ensuring a well-rounded educational
-                        experience.
+                     <p>Welcome to the Courses page of Uday Pratap Inter College, where we offer a diverse range of academic 
+                     programs designed to nurture the holistic development of our students. Our institution is committed to providing 
+                     high-quality education that empowers students with knowledge, critical thinking skills, and a passion for learning. From science and mathematics to humanities and arts, our comprehensive curriculum caters to a wide spectrum of interests. Our experienced and dedicated faculty members create an engaging learning environment, fostering intellectual curiosity and a thirst for knowledge. At Uday Pratap Inter College, we prioritize innovation and technology integration, ensuring that our students are well-equipped for the challenges of the modern world. Explore our courses to discover a wealth of opportunities for academic excellence, personal growth, and future success. Whether it's preparing for competitive exams or pursuing creative endeavors, our courses lay the foundation for a bright and promising future. Join us on this educational journey where every 
+                     course is a stepping stone towards a well-rounded and fulfilling academic experience at Uday Pratap Inter College.
                      </p>
                   </div>
                </div>
