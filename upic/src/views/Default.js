de2745ea9a6afd55e531c512cd616d08df09fa8f
Saurav Pandey <saurav.pandey@corotsystems.com>
2023-12-20 17:45:43 +0530

    Update Default.js

--- upic/src/views/Default.js
+++ upic/src/views/Default.js
@@ -6,10 +6,7 @@ import ServivesProcess from '../components/ServivesProcess';
 import WhatClientSay from '../components/WhatClientSay';
 import WhyChoose from '../components/WhyChoose';
 import Welcome from '../components/Welcome';
-<<<<<<< HEAD
-=======
 import Faculty from '../components/Faculty';
->>>>>>> d95ff8c4155d8854c1bab14bb044a81c7abe6b61
 const Default = () => {
   return (
     <>
