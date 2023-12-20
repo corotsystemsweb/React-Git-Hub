a44b662db5b189cf4f34d1e81a9437ff818a7b1e
Ankita <ankitaojha1611@gmail.com>
2023-12-20 11:14:52 +0530

    Class Update

--- upic/src/views/AboutUs.js
+++ upic/src/views/AboutUs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AboutImg from "../assets/images/about.png";
 import About2 from "../assets/images/banner.jpeg";
 import PageTopBanner from '../components/PageTopBanner';
+import "../assets/css/style.css";
 
 
 const AboutUs = () => {
@@ -12,7 +13,7 @@ const AboutUs = () => {
             <div className="container">
                <div className="row">
                   <div className="col-md-12 text-center">
-                     <h2 classNameName='page-head'>About Us</h2>
+                     <h2 className='page-head'>About Us</h2>
                      <p>Welcome to Uday Pratap Inter College, where academic excellence meets holistic development.
                         Our institution is committed to nurturing young minds, fostering a love for learning, and empowering
                         students with the knowledge and skills to navigate a dynamic world.</p>
@@ -22,7 +23,7 @@ const AboutUs = () => {
                <div className="row mt-5">
                   <div className="col-md-6 ">
                      <div className="about_box mt-5">
-                        <figure><img src={About2} alt="About Green Computers" /></figure>
+                        <figure><img src={About2} alt="About Us" className='img-shadow'/></figure>
                      </div>
                   </div>
                   <div className="col-md-6">
@@ -35,9 +36,7 @@ const AboutUs = () => {
                         in our students. At Uday Pratap Inter College, we believe in empowering every student to discover their unique
                         potential, equipping them with the skills needed to navigate an ever-changing world. Our commitment to academic
                         excellence is complemented by a rich tapestry of co-curricular activities, ensuring a well-rounded educational
-                        experience. As we embark on a journey of shaping future leaders, Uday Pratap Inter College remains steadfast in
-                        its mission to inspire, educate, and transform lives. Join us in this pursuit of knowledge,
-                        character, and success, as we continue to illuminate the path towards a brighter future for every Udayan.
+                        experience. 
                      </p>
                   </div>
                </div>
@@ -48,8 +47,7 @@ const AboutUs = () => {
                <div className="row mt-5">
                   <div className="col-md-6">
                      <p>
-                        Uday Pratap Inter College is not merely an academic space; it is a vibrant community that fosters creativity, 
-                        critical thinking, and character development. Our experienced faculty members are committed to guiding students on
+                         Our experienced faculty members are committed to guiding students on
                          their educational journey, cultivating a positive and enriching environment.
                         At Uday Pratap Inter College, we believe in nurturing well-rounded individuals, emphasizing both academic prowess and extracurricular achievements. Our state-of-the-art facilities, coupled with a diverse range of programs, create an atmosphere where students can explore their talents and interests. As we celebrate a rich heritage of educational excellence, we continuously strive to adapt and evolve, ensuring our students are well-prepared for the challenges of the modern world. Join us at Uday Pratap Inter College,
                          where education goes beyond textbooks, and every student is empowered to reach their full potential.
@@ -57,7 +55,7 @@ const AboutUs = () => {
                   </div>
                   <div className="col-md-6 ">
                      <div className="about_box mt-5">
-                        <figure><img src={About2} alt="About Green Computers" /></figure>
+                        <figure><img src={About2} alt="About Us" className='img-shadow'/></figure>
                      </div>
                   </div>
                </div>
