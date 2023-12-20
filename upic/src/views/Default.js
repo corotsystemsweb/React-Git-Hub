890e6028978214c13dee3d4475cb47c2fe0d9d8d
Saurav Pandey <saurav.pandey@corotsystems.com>
2023-12-20 17:42:50 +0530

    faculty components added

--- upic/src/views/Default.js
+++ upic/src/views/Default.js
@@ -5,11 +5,7 @@ import RequestAFreeQuote from '../components/RequestAFreeQuote';
 import ServivesProcess from '../components/ServivesProcess';
 import WhatClientSay from '../components/WhatClientSay';
 import WhyChoose from '../components/WhyChoose';
-<<<<<<< Updated upstream
 import Welcome from '../components/Welcome';
-=======
-import Faculty  from '../components/Faculty';
->>>>>>> Stashed changes
 const Default = () => {
   return (
     <>
@@ -19,12 +15,7 @@ const Default = () => {
       <WhyChoose />
       <ServivesProcess />
       <WhatClientSay />
-<<<<<<< Updated upstream
       <RequestAFreeQuote /> */}
-=======
-      <RequestAFreeQuote />
-      <Faculty />
->>>>>>> Stashed changes
     </>
   )
 }
