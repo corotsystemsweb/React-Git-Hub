51061372ded716b9682ff845431bf9f4e6c7928d
Saurav Pandey <saurav.pandey@corotsystems.com>
2023-12-21 18:26:43 +0530

    carousel slider updated

--- upic/src/components/HomeSlider.js
+++ upic/src/components/HomeSlider.js
@@ -1,72 +1,36 @@
 import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 import SliderImg1 from "../assets/images/banner-01.jpg";
-import SliderImg1Mob from "../assets/images/banner-about.jpg";
-import SliderImg2 from "../assets/images/banner2.jpg";
-import SliderImg2Mob from "../assets/images/banner2-mobile.jpg";
-import SliderImg3 from "../assets/images/banner3.jpg";
-import SliderImg3Mob from "../assets/images/banner3-mobile.jpg";
+import SliderImg2 from "../assets/images/banner-02.jpg";
+import SliderImg3 from "../assets/images/banner-03.jpg";
 
 const HomeSlider = () => {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,       // Enable automatic sliding
+    autoplaySpeed: 200,  // Set the duration in milliseconds (0.2 seconds in this case)
+  };
+
   return (
-   
-    <section className="slider_section">
-    <div id="main_slider" className="carousel slide banner-main" data-ride="carousel">
-
-       <div className="carousel-inner">
-          <div className="carousel-item active">
-             <picture>
-               <source media="(max-width: 650px)" srcset={SliderImg1Mob} />
-               <source media="(max-width: 465px)" srcset={SliderImg1Mob} />
-               <img className="first-slide"  src={SliderImg1} alt="First slide" />
-            </picture>
-             {/* <div className="container">
-                <div className="carousel-caption relative">
-                   <h1> <br /> <strong className="black_bold"></strong><br />
-                      <strong className="yellow_bold">UDAY PRATAP INTER COLLEGE</strong></h1>
-                   <p>&nbsp;</p>
-                   
-                </div>
-             </div> */}
-          </div>
-          <div className="carousel-item">
-          <picture>
-               <source media="(max-width: 650px)" srcset={SliderImg2Mob} />
-               <source media="(max-width: 465px)" srcset={SliderImg2Mob} />
-               <img className="second-slide" src={SliderImg2} alt="Second slide" />
-            </picture>
-            
-             <div className="container">
-                <div className="carousel-caption relative">
-                </div>
-             </div>
-          </div>
-          <div className="carousel-item">
-          <picture>
-               <source media="(max-width: 650px)" srcset={SliderImg3Mob} />
-               <source media="(max-width: 465px)" srcset={SliderImg3Mob} />
-               <img className="third-slide"  src={SliderImg3} alt="Third slide" />
-            </picture>
-            
-             <div className="container">
-                <div className="carousel-caption relative">
-                   <br />
-                </div>
-             </div>
-          </div>
-
-       </div>
-       <a className="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
-       <i className='fa fa-angle-right'></i>
-       </a>
-       <a className="carousel-control-next" href="#main_slider" role="button" data-slide="next">
-       <i className='fa fa-angle-left'></i>
-       </a>
-       
-    </div>
-
- </section>
-
-  )
-}
+    <Slider {...sliderSettings}>
+      <div>
+        <img src={SliderImg1} alt="Slide 1" />
+      </div>
+      <div>
+        <img src={SliderImg2} alt="Slide 2" />
+      </div>
+      <div>
+        <img src={SliderImg3} alt="Slide 3" />
+      </div>
+    </Slider>
+  );
+};
 
 export default HomeSlider;
