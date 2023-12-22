4257d0a060d22f436a5f8bd416203622ba324a82
Ankita <ankitaojha1611@gmail.com>
2023-12-22 17:06:21 +0530

    footer updated

--- upic/src/components/Footer.js
+++ upic/src/components/Footer.js
@@ -1,32 +1,22 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 const Footer = () => {
-  return (
-    <footer>
-         <div className="footer">
+   return (
+      <footer>
+         <div className="footer mt-5">
             <div className="container">
                <div className="row">
-                  <div className="col-md-6 offset-md-3">
-                     <ul className="sociel">
-                         <li> <a href="#"><i className="fa fa-facebook-f"></i></a></li>
-                         <li> <a href="#"><i className="fa fa-twitter"></i></a></li>
-                         <li> <a href="#"><i className="fa fa-instagram"></i></a></li>
-                         <li> <a href="#"><i className="fa fa-instagram"></i></a></li>
-                     </ul>
+                  <div className="col-md-4 ">
+                     <div className="contact">
+                        <h4>Uday Pratap Inter College</h4>
+                        <span> Uday Pratap Inter College, a beacon of knowledge and excellence in education.Nestled in the heart of our vibrant community, we take pride in nurturing young minds.</span>
+                     </div>
                   </div>
-            </div>
-            <div className="row">
-            <div className="col-md-4 ">
-                  <div className="contact">
-                     <h3>Uday Pratap Inter College</h3>
-                     <span> </span>
-                  </div>
-               </div>
-               <div className="col-md-4 ">
-                  <div className="contact">
-                     <h3>Useful Links</h3>
-                     <ul className="lik">
-                           <li> <a href="/about-us">About Us</a></li> 
+                  <div className="col-md-4 ">
+                     <div className="contact">
+                        <h4>Useful Links</h4>
+                        <ul className="lik">
+                           <li> <a href="/about-us">About Us</a></li>
                            <li> <a href="/course">Course</a></li>
                            <li> <a href="/photo-gallery">Photo Gallery</a></li>    
                            <li> <a href="/contact-us">Contact Us</a></li> 
@@ -40,22 +30,28 @@ const Footer = () => {
                       <br />
                         <br />
                        <br />
-                        Phone -
+                        Phone - 9886658489 
                         </span>
+                        <hr />
+                     </div>
+
                   </div>
+                  <div className='col-md-12 text-center'>
+                  <p className="copyright">© 2023 Uday Pratap Inter College. All Right Reserved. Designed and Developed By <a href='https://www.corotsystems.com/' target='_blanck'>Corot Systems.</a></p>
+
+                  </div>
+
+
+
                </div>
-                 
-                
-                
             </div>
+            {/* <div className="copyright">
+               <p className="copyright">© 2023 Uday Pratap Inter College. All Right Reserved. Designed and Developed By <a href='https://www.corotsystems.com/' target='_blanck'>Corot Systems.</a></p>
+            </div> */}
+
          </div>
-            <div className="copyright">
-               <p>© 2023 Uday Pratap Inter College. All Right Reserved. Designed and Developed By <a href='https://www.corotsystems.com/' target='_blanck'>Corot Systems.</a></p>
-            </div>
-         
-      </div>
       </footer>
-  )
+   )
 }
 
 export default Footer;
